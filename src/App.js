b9951f34a3018da67e8d8c67126c37b1416ef24f
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-04-08 16:17:19 +0200

    renamed function getting props from local storage, put the function outside the App

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import Form from "./Form";
 import Buttons from "./Buttons";
 import Tasks from "./Tasks";
 
+const getInitialTasks = () => {
+	const tasksFromLocalStorage = localStorage.getItem("tasks");
+
+	return tasksFromLocalStorage
+		? JSON.parse(tasksFromLocalStorage)
+		: [];
+};
+
 function App() {
-	const getLocalStorage = () => {
-		const localStorageTasks = localStorage.getItem("tasks");
-
-		return localStorageTasks
-			? JSON.parse(localStorageTasks)
-			: [];
-	};
-
 	const [hideDone, setHideDone] = useState(false);
-	const [tasks, setTasks] = useState(getLocalStorage);
+	const [tasks, setTasks] = useState(getInitialTasks);
 
 	useEffect(() => {
 		localStorage.setItem("tasks", JSON.stringify(tasks));
