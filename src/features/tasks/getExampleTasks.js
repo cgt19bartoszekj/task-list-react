483a3048b736dd731ff4a33824d97d6accc53f3e
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-05-12 14:54:21 +0200

    downloading example tasks

---
+++ src/features/tasks/getExampleTasks.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const getExampleTasks = async () => {
+
+  try {
+    const response = await axios("/task-list-react/exampleTasks.json");
+    return await response.data;
+  }
+  catch (err) {
+    console.log(err);
+  }
+
+};
