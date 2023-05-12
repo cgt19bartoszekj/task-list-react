d5fe07d5312be0fc99f302b75f3d83a0198b02da
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-05-12 14:56:04 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
 
-const App = () => <Tasks />;
+const App = () => (
+  <BrowserRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">Zadania</Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route
+        path="/zadania"
+        element={<Tasks />}>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
