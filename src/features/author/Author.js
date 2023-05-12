9a43beb9a2c5fca6d152f83565e0bb4c3e2a0ac5
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-05-12 14:56:09 +0200

    created author page

---
+++ src/features/author/Author.js
@@ -0,0 +1,13 @@
+import { Container } from "../../common/Container/styled";
+import { Header } from "../../common/Header";
+import { Section } from "../../common/Section";
+
+const Author = () => (
+  <Container>
+    <Header title="O autorze"/>
+    <Section>
+    </Section>
+  </Container>
+);
+
+export default Author;
