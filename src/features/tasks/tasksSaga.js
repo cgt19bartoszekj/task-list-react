23b5eb426edd1e96184a4b89876c8b2458c917a6
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-05-12 14:53:45 +0200

    created tasksSaga

---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,25 @@
+import { put, call, takeLatest, delay, takeEvery, select } from 'redux-saga/effects';
+import { fetchExampleTasks, selectTasks, setTasks } from "./tasksSlice";
+import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocalStorage } from './tasksLocalStorage';
+
+function* fetchExampleTasksHandler() {
+  try {
+    yield delay(1000);
+    const exampleTasks = yield call(getExampleTasks);
+    yield put(setTasks(exampleTasks));
+  }
+  catch (error) {
+    yield console.log();
+  }
+}
+
+function* saveTasksInLocalStorageHandler() {
+  const tasks = yield select(selectTasks);
+  yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* tasksSaga() {
+  yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+  yield takeEvery("*", saveTasksInLocalStorageHandler);
+}
