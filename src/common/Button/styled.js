e5f8071b6e5ddf4c0f41fbdd5a452eb418ee1ab3
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-05-15 18:02:09 +0200

    fixed hover color on button

--- src/common/Button/styled.js
+++ src/common/Button/styled.js
@@ -1,18 +1,18 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-  cursor: pointer;
-  background: none;
-  border: none;
-  color: ${({ theme }) => theme.colors.teal};
-  transition: 1s;
+	cursor: pointer;
+	background: none;
+	border: none;
+	color: ${({ theme }) => theme.colors.teal};
+	transition: 1s;
 
-  &:hover {
-    color: ${({ theme }) => theme.colors.eal};
-  }
+	&:hover {
+		color: ${({ theme }) => theme.colors.brightTeal};
+	}
 
-  &:disabled {
-    color: #b3b3b3;
-    cursor: default;
-  }
+	&:disabled {
+		color: #b3b3b3;
+		cursor: default;
+	}
 `;
